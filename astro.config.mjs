d998b6cb1509b87726128c8cc0e030c74521b579
Mauricio Ferreira <mauriciovictor17@gmail.com>
2025-10-15 11:25:27 -0300

    chore: configurando netfly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-
+import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
+    adapter: netlify(),
 });
