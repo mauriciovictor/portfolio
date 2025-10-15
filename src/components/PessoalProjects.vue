6af7191ea2f7f93c5b9db970be9ce61f14f054ba
Mauricio Ferreira <mauriciovictor17@gmail.com>
2025-10-15 14:35:59 -0300

    style: padronizando fundo dos modals

--- src/components/PessoalProjects.vue
+++ src/components/PessoalProjects.vue
@@ -26,7 +26,7 @@
     <!-- Modal -->
     <div
         v-if="showModal"
-        class="fixed inset-0 z-50 flex items-center justify-center bg-black/70 backdrop-blur-sm"
+        class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm"
         @click.self="closeModal"
     >
       <div
