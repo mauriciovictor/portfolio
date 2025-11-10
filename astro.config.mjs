52bb1ac402b9b804052e818e5c5af564e6762c32
Mauricio Ferreira <mauriciovictor17@gmail.com>
2025-11-10 15:02:00 -0300

    feature: config github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import vue from '@astrojs/vue';
-import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
-    adapter: netlify(),
+    // Configurado automaticamente a partir do remote git: mauriciovictor/portfolio
+    site: 'https://mauriciovictor.github.io/portfolio',
+    base: '/portfolio/',
+    integrations: [vue()],
 });
